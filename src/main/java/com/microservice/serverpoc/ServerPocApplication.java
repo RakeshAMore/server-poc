0f06997bdfb9aa529992e360ca04694a6dd4276a
Rakeshkumar More <rakeshkumar.more@synacor.com>
2022-11-03 18:30:35 +0530

    init

---
+++ src/main/java/com/microservice/serverpoc/ServerPocApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.serverpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/serverpoc/ServerPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.serverpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
