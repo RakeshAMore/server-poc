be22a16a006042f3cd55092a772a77f68eaff755
Rakeshkumar More <rakeshkumar.more@synacor.com>
2022-11-11 22:24:03 +0530

    adding some change

--- src/main/java/com/microservice/serverpoc/utils/SalaryCalculator.java
+++ src/main/java/com/microservice/serverpoc/utils/SalaryCalculator.java
@@ -5,4 +5,8 @@ public class SalaryCalculator {
     public double calculate(int noOfPaidDays, double perDay) {
         return noOfPaidDays * perDay;
     }
+
+    public double calculate2(int noOfPaidDays, double perDay) {
+        return noOfPaidDays * perDay + 100;
+    }
 }
