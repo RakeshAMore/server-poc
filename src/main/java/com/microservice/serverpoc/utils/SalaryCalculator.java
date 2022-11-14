34431ad3e26bb322fb6dad3671d04afec226e299
Rakeshkumar More <rakeshkumar.more@synacor.com>
2022-11-14 09:09:40 +0530

    Bad commit

--- src/main/java/com/microservice/serverpoc/utils/SalaryCalculator.java
+++ src/main/java/com/microservice/serverpoc/utils/SalaryCalculator.java
@@ -9,4 +9,25 @@ public class SalaryCalculator {
     public double calculate2(int noOfPaidDays, double perDay) {
         return noOfPaidDays * perDay + 100;
     }
+
+    public double calculate3(int noOfPaidDays, double perDay) {
+        return noOfPaidDays * perDay + 100;
+    }
+
+
+    public double calculate4(int noOfPaidDays, double perDay)
+
+
+
+
+    {
+        boolean someFakeCondition =           Boolean.TRUE;
+        if
+        ( someFakeCondition) {
+            System.out.println("hellooo");
+        }
+
+
+        return noOfPaidDays * perDay + 100;
+    }
 }
